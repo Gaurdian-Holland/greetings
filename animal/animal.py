37fb3edec7afe2a78599f16944d6547621a29c30
Veronica <76923323+Gaurdian-Holland@users.noreply.github.com>
2021-01-07 03:01:24 -0500

    Add files via upload

---
+++ animal/animal.py
@@ -0,0 +1,7 @@
+# Author: McKenzie Holland
+# Date: 1/6/2021
+# Description: Asks the user for their favorite animal and then
+#               prints out what is their favorite animal.
+
+fave_animal = input("Please enter your favorite animal:")
+print("Your favorite animal is the" + fave_animal + ".")
